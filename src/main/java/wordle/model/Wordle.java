a8fc3ff730703a38708c409b56165de1abd4c094
JonFHq <fernandez.hujon21@zaragoza.salesianos.edu>
2023-02-08 11:10:47 +0100

    11:10

---
+++ src/main/java/wordle/model/Wordle.java
@@ -0,0 +1,8 @@
+package wordle.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Wordle {
+    
+}
---
+++ src/main/java/wordle/service/WordleService.java
@@ -0,0 +1,8 @@
+package wordle.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordleService {
+    
+}
